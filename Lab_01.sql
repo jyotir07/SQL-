cf1646a2136034cb5a245ea17eec3c894eef03a9
Jyotiraditya Singh <77264025+jyotir07@users.noreply.github.com>
2024-02-22 09:45:08 +0530

    Create Lab_01.sql

---
+++ Lab_01.sql
@@ -0,0 +1,9 @@
+-- Author: jyotir07
+--Creating a table in an existing database 
+Create table jyotirs_table(
+  personal_id int,
+  name varchar,
+  age int
+);
+-- Above program creates a table named jyotirs_table
+-- There are 3 columns in the table, and their datatype is defined
